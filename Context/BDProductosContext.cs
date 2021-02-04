7142f03d6f0de485bf93616e71c3aeded6b8d759
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2021-02-04 10:29:32 -0500

    Agregando Clase Usuario + Context

---
+++ Context/BDProductosContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API_Login_CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Login_CRUD.Context
+{
+    public class BDProductosContext : DbContext
+    {
+        public BDProductosContext(DbContextOptions<BDProductosContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Usuario> usuarios { get; set; } 
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Login_CRUD.Models
+{
+    [Table("USUARIO")]
+    public class Usuario
+    {
+        [Key]
+        public int id_user { get; set; }
+        public string ussername { get; set; }
+        public string pass { get; set; }
+        public string nombre { get; set; }
+        public string apellido_paterno { get; set; }
+        public string apellido_materno { get; set; }
+        public string correo { get; set; }
+    }
+}
