d0b7579219d22ed739e45bb39f8027b140805609
sebastian-reyes <sebastian_reyes_2105@hotmail.com>
2021-02-05 10:04:18 -0500

    Creando clase Productos

---
+++ Models/Productos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API_Login_CRUD.Models
+{
+    [Table("PRODUCTOS")]
+    public class Productos
+    {
+        [Key]
+        public int id_prod { get; set; }
+        public string nombre_prod { get; set; }
+        public string desc_prod { get; set; }
+        public int stock_min { get; set; }
+        public int stock_actual { get; set; }
+        public double precio { get; set; }
+    }
+}
